d55f4d3d913db335c0327ad70b6364aa681ff330
giangdo11 <dogiang09@gmail.com>
2024-11-26 00:52:45 +0700

    Update shape-detection.py

--- shape-detection.py
+++ shape-detection.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 
+import sys
+import argparse
+
+# print("warning:  importing Jetson.Inference is deprecated.  please 'import jetson_inference' instead.")
 from jetson_inference import detectNet
-from jetson_utils import videoSource, videoOutput
+from jetson_utils import videoSource, videoOutput, Log
 
-# net = detectNet("ssd-mobilenet-v2", threshold=0.5)
-net = detectNet(argv=["--model=/shape-detection/models/ssd-mobilenet.onnx", "--labels=/shape-detection/models/labels.txt", "--input-blob=input_0", "--output-cvg=scores", "--output-bbox=boxes"], threshold=0.5)
-# camera = videoSource("csi://0")      # '/dev/video0' for V4L2
-camera = videoSource("/dev/video0")      # '/dev/video0' for V4L2
-display = videoOutput("display://0") # 'my_video.mp4' for file
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=detectNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
 
-while display.IsStreaming():
-    img = camera.Capture()
+parser.add_argument("path", type=str, default="", nargs='?', help="The path to onnx model")
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# create video sources and outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+
+net = detectNet(argv=[f"--model={args.path}/ssd-mobilenet.onnx", f"--labels={args.path}/labels.txt", "--input-blob=input_0", "--output-cvg=scores", "--output-bbox=boxes"], threshold=0.5)
+
+while True:
+    img = input.Capture()
 
     if img is None: # capture timeout
         continue
@@ -18,5 +38,5 @@ while display.IsStreaming():
     detections = net.Detect(img)
     print(detections)
     
-    display.Render(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+    output.Render(img)
+    output.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
